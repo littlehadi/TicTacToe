9cba19547f269ba8f4a7c7a4792d7e62942ca992
Hady <Helsa074@uottawa.ca>
2021-05-02 03:03:20 -0500

    Create the Board and the vertical lines

--- Main.cpp
+++ Main.cpp
@@ -3,17 +3,24 @@
 using namespace std;
 
 
-
-char board[3][3] = {{'1','2', '3'},{'4','5','6'}, {'7','8','9'}}; // Create the board
+// Create the board
+char board[3][3] = {{'1','2', '3'},{'4','5','6'}, {'7','8','9'}}; 
 
 void drawBoard()
 { 
-	cout << " " << board[0][0] << " | " << board[0][1] <<  " | " << board[0][2] << endl;// Create the first row with the bar
+	// Create the vertical and horizontal lines
+	cout << " " << board[0][0] << " | " << board[0][1] <<  " | " << board[0][2] << endl;
+	cout << "-----------\n";
+	cout << " " << board[1][0] << " | " << board[1][1] <<  " | " << board[1][2] << endl;
+	cout << "-----------\n";
+	cout << " " << board[2][0] << " | " << board[2][1] <<  " | " << board[2][2] << endl;
+	cout << "-----------\n";
 
 }
 
 int main () {
 	cout << "Hello World \n";
+	drawBoard();
 
   return 0;
 }
