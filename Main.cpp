214897d618e9bb668631b00243464dd8ddf3b3fa
Hady <Helsa074@uottawa.ca>
2021-05-02 02:32:27 -0500

    Create the Board and the vertical lines

--- Main.cpp
+++ Main.cpp
@@ -2,9 +2,18 @@
 #include <iostream>
 using namespace std;
 
-int main () {
 
-  cout << "Hello World \n";
+
+char board[3][3] = {{'1','2', '3'},{'4','5','6'}, {'7','8','9'}}; // Create the board
+
+void drawBoard()
+{ 
+	cout << " " << board[0][0] << " | " << board[0][1] <<  " | " << board[0][2] << endl;// Create the first row with the bar
+
+}
+
+int main () {
+	cout << "Hello World \n";
 
   return 0;
 }
