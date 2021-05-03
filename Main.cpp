9083345930341f03980f555dc1e286d7070636d8
Hady <Helsa074@uottawa.ca>
2021-05-03 00:20:37 -0500

    Created the game interface

--- Main.cpp
+++ Main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 char board[3][3] = {{'1','2', '3'},{'4','5','6'}, {'7','8','9'}}; 
 
 char current_marker;
-int current;
+int current_player;
 
 void drawBoard()
 { 
@@ -24,24 +24,91 @@ void drawBoard()
 void placeMarker(int slot) {
 	int row = slot / 3;
 	int col;
+	// If slot is a multiple of 3
 	if (slot % 3 == 0) {
-
 		row =row -1;
 		col = 2;
 	}
 	else {
-	col = (slot % 3) -1;
+		col = (slot % 3) -1;
 	}
+
 	board[row][col] = current_marker;
 
 }
 
+//Check if we have a winner
+char winner() {
+	for (int i =0; i< 3; i++) {
+		// check the rows
+		if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+			return current_player;
+		}
+		// check the col
+		if (board[0][1] == board[1][i] && board[1][i] == board[2][i]) {
+			return current_player;
+		}
+	}
+
+	//check the diagonal
+	if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+		return current_player;
+	}
+
+	if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+		return current_player;
+	}
+	// If no one wins, return 0
+	return 0;
+
+}
+// A function that swap the player and the marker in the game
+void swap_player_and_marker() {
+	
+	if (current_marker == 'x'){
+		current_marker = 'O';
+	}
+	else {
+		current_marker = 'X';
+	}
+
+	if (current_player ==1) {
+		current_player = 2;
+	}
+	else {
+		current_player = 1;
+	}
+}
+
+
+// creating the interface for the game
+void game () {
+	cout << '\n';
+	cout << "Player one, choose your marker : "; 
+	char marker_p1;
+	cin >> marker_p1;
+	current_player = 1;
+	current_marker = marker_p1;
+
+	/*cout << "Player two, choose your marker : "; 
+	char marker_p2;
+	cin >> marker_p2;
+	current_player = 2;
+*/
+	drawBoard();
+
+	for (int i = 0; i < 9; i++) {
+		cout << "It's player " << current_player << "'s turn. Enter your slot";
+		int slot;
+		cin >> slot;
+
+		placeMarker(slot);
+		drawBoard();
+	}
+}
+
 
 int main () {
-	//cout << "Hello World \n";
-	drawBoard();
-	cout << '\n';
-	current_marker = 'X';
-	placeMarker(9);
-	drawBoard();
+	game();
+
 }
